e423018bd770b746900730bae693156e39e64b36
PavelDusek <pavel.dusek@gmail.com>
2023-12-10 08:03:54 +0100

    pavucontrol

--- screencapture.sh
+++ screencapture.sh
@@ -1,3 +1,5 @@
+pavucontrol
+
 #all audio inputs (with microphone):
 ffmpeg -f x11grab -video_size 1360x768 -framerate 25 -i $DISPLAY -f alsa -i default -c:v libx264 -pix_fmt yuv420p -preset ultrafast -c:a aac $1 
 
